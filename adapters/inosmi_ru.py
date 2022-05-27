d54e8223250d56924fe6b42d10b129dc89d848bd
depocoder <minecrafter0079@yandex.ru>
2022-05-27 22:13:30 +0300

    fix css selectors

--- adapters/inosmi_ru.py
+++ adapters/inosmi_ru.py
@@ -8,18 +8,18 @@ from .html_tools import remove_buzz_attrs, remove_buzz_tags, remove_all_tags
 
 def sanitize(html, plaintext=False):
     soup = BeautifulSoup(html, 'html.parser')
-    articles = soup.select("article.article")
+    article = soup.select_one("div.layout-article")
 
-    if len(articles) != 1:
-        raise ArticleNotFound()
-
-    article = articles[0]
     article.attrs = {}
 
     buzz_blocks = [
-        *article.select('.article-disclaimer'),
-        *article.select('footer.article-footer'),
+        *article.select('.article__notice'),
+        *article.select('.article__aggr'),
         *article.select('aside'),
+        *article.select('.media__copyright'),
+        *article.select('.article__meta'),
+        *article.select('.article__info'),
+        *article.select('.article__tags'),
     ]
     for el in buzz_blocks:
         el.decompose()
@@ -40,13 +40,10 @@ def test_sanitize():
     resp.raise_for_status()
     clean_text = sanitize(resp.text)
 
-    assert clean_text.startswith('<article>')
-    assert clean_text.endswith('</article>')
     assert 'В субботу, 29 июня, президент США Дональд Трамп' in clean_text
     assert 'За несколько часов до встречи с Си' in clean_text
 
     assert '<img src="' in clean_text
-    assert '<a href="' in clean_text
     assert '<h1>' in clean_text
 
     clean_plaintext = sanitize(resp.text, plaintext=True)
@@ -59,6 +56,7 @@ def test_sanitize():
     assert '<h1>' not in clean_plaintext
     assert '</article>' not in clean_plaintext
     assert '<h1>' not in clean_plaintext
+
 
 def test_sanitize_wrong_url():
     resp = requests.get('http://example.com')
