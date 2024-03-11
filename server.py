2b8eda9a1bc6418c0b5a1b49e951a1c26d63a7b1
anton <a.solovjev@vzv.su>
2024-03-11 13:09:49 +0300

    added tests

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from functools import partial
 
+import pytest
 from aiohttp import web
 
-from main import read_file, main
+from process import read_file, process_article_main, ProcessingStatus, get_charged_words
 
 
 async def handle(charged_words, request, max_urls=10):
@@ -15,10 +16,45 @@ async def handle(charged_words, request, max_urls=10):
         error_message = {'error': f'too many urls in request, should be {max_urls} or less'}
         return web.json_response(error_message, status=400)
 
-    result_articles = await main(urls_list, charged_words)
+    result_articles = await process_article_main(urls_list, charged_words)
     response_data = [{'status': status, 'url': url, 'score': rating, 'words_count': word_count}
                      for url, status, rating, word_count, _ in result_articles]
     return web.json_response(response_data)
+
+
+@pytest.fixture
+def dummy_charged_words():
+    negative_test_words_path = 'charged_dict/negative_words.txt'
+    positive_test_words_path = 'charged_dict/positive_words.txt'
+    charged_words = get_charged_words(negative_test_words_path, positive_test_words_path)
+    return charged_words
+
+
+@pytest.mark.asyncio
+async def test_process_article_main_successful(dummy_charged_words):
+    test_articles = ['https://inosmi.ru/20240214/241584580.html']
+    result_articles = await process_article_main(test_articles, dummy_charged_words)
+    assert len(result_articles) == 1
+    assert result_articles[0][0] == test_articles[0]
+    assert result_articles[0][1] == ProcessingStatus.OK.value
+
+
+@pytest.mark.asyncio
+async def test_process_article_main_fetch_error(dummy_charged_words):
+    test_articles = ['https://inosmi.ru/not/exist.html']
+    result_articles = await process_article_main(test_articles, dummy_charged_words)
+    assert len(result_articles) == 1
+    assert result_articles[0][0] == test_articles[0]
+    assert result_articles[0][1] == ProcessingStatus.FETCH_ERROR.value
+
+
+@pytest.mark.asyncio
+async def test_process_article_main_parsing_error(dummy_charged_words):
+    test_articles = ['https://example.com']
+    result_articles = await process_article_main(test_articles, dummy_charged_words)
+    assert len(result_articles) == 1
+    assert result_articles[0][0] == test_articles[0]
+    assert result_articles[0][1] == ProcessingStatus.PARSING_ERROR.value
 
 
 if __name__ == '__main__':
